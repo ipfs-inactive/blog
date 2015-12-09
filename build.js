3b6ba24aacb6ac2d90619bbd89d28d10a40aa782
David Dias <daviddias.p@gmail.com>
2015-12-09 14:05:12 -0800

    standard

--- build.js
+++ build.js
@@ -4,14 +4,13 @@ var Metalsmith = require('metalsmith')
 var debug = require('metalsmith-debug')
 var templates = require('metalsmith-templates')
 var collections = require('metalsmith-collections')
-var partial = require('metalsmith-partial')
-var msstatic = require('metalsmith-static')
 var serve = require('metalsmith-serve')
 var watch = require('metalsmith-watch')
 var markdown = require('metalsmith-markdown')
-var headingsidentifier = require("metalsmith-headings-identifier");
 var permalinks = require('metalsmith-permalinks')
 var feed = require('metalsmith-feed')
+var msstatic = require('metalsmith-static')
+var headingsidentifier = require('metalsmith-headings-identifier')
 
 var nunjucks = require('nunjucks')
 var njmd = require('nunjucks-markdown')
@@ -21,13 +20,13 @@ var marked = require('marked')
 marked.setOptions({
   gfm: true,
   tables: true,
-  smartLists: true,
+  smartLists: true
 })
 
-njenv = nunjucks.configure()
+var njenv = nunjucks.configure()
 njmd.register(njenv, marked)
 
-njdate.setDefaultFormat('YYYY-MM-DD, h:mm:ss a');
+njdate.setDefaultFormat('YYYY-MM-DD, h:mm:ss a')
 njdate.install(njenv)
 
 njenv.addFilter('dump', JSON.stringify)
@@ -36,30 +35,30 @@ Metalsmith(__dirname)
   .use(debug())
   .metadata({
     site: {
-      title: "IPFS Blog",
-      url: "http://ipfs.io/blog/",
-      author: "The IPFS Team",
-    },
+      title: 'IPFS Blog',
+      url: 'http://ipfs.io/blog/',
+      author: 'The IPFS Team'
+    }
   })
   .use(collections({
-    posts: {},
+    posts: {}
   }))
   .use(markdown())
-  .use(templates({ "directory": ".", "engine": "nunjucks", "inPlace": true }))
-  .use(templates({ "directory": ".", "engine": "nunjucks" }))
+  .use(templates({ 'directory': '.', 'engine': 'nunjucks', 'inPlace': true }))
+  .use(templates({ 'directory': '.', 'engine': 'nunjucks' }))
   .use(headingsidentifier())
   .use(permalinks())
-  .use(feed({"collection": "posts"}))
-  .use(msstatic({"src": "tmpl/static", "dest": "static"}))
+  .use(feed({'collection': 'posts'}))
+  .use(msstatic({'src': 'tmpl/static', 'dest': 'static'}))
   .use(serve({
-    "port": 8081,
-    "verbose": true
+    'port': 8081,
+    'verbose': true
   }))
   .use(watch())
   .destination('./build')
-  .build(function(err){
+  .build(function (err) {
     if (err) {
       console.log(err)
-      throw err;
+      throw err
     }
   })
