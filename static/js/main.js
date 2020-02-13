c1fb4423095d29d629c77f894f9ad571ada14849
Marcin Rataj <lidel@lidel.org>
2020-02-13 18:37:34 +0100

    refactor: ..IE is fine too

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,4 @@
 // Fix ipfs.io/blog/* links loaded via DNSLink: https://github.com/ipfs/blog/issues/360
-if (window.location.hostname === 'ipfs.io' && window.location.pathname.startsWith('/blog')) {
+if (window.location.hostname === 'ipfs.io' && window.location.pathname.indexOf('/blog') === 0) {
   window.location.replace(window.location.href.replace('//ipfs.io/blog', '//blog.ipfs.io'))
 }
